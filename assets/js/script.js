f6b6210fcd299532013d33502fc99707f3f71ea1
consoekamti <167833648+consoekamti@users.noreply.github.com>
2024-10-11 00:33:11 +0700

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["PLC Programmers", "Mechanics Design", "3D & 2D Sketch", "Electrician", "Programmers"],
+    strings: ["PLC Programmers", "Mechanics Design", "3D n 2D Sketch", "Electrician", "Programmers"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
